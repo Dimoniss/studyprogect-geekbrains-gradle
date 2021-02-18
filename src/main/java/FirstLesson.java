b16b64bb4c092cba9c6aa85bea5878d9f2cbcd5b
TecAssistProMaster <tecassistpro@gmail.com>
2021-02-18 20:00:25 +0100

    Task 2: Create variables of all passed data types and initialize their values.

--- src/main/java/FirstLesson.java
+++ src/main/java/FirstLesson.java
@@ -2,6 +2,31 @@
 
 public class FirstLesson {
     public static void main(String[] args) {
-        System.out.println("Hello world !!!");
+
+        // from -128 to 127 and takes 1 byte
+        byte b = 111;
+
+        // from -32768 to 32767 and takes 2 byte
+        short s = 22222;
+
+        // from -2147483648 to 2147483647 and takes 4 byte
+        int i = 100000000;
+
+        // from –9 223 372 036 854 775 808 to 9 223 372 036 854 775 807 and takes 8 byte
+        long  l = 999999999999999999L;
+
+        //stores true or false
+        boolean isGood = true;
+
+        //stores a floating point number from ± 4.9 * 10-324 to ± 1.8 * 10308 and occupies 8 bytes
+        double d = 3.14;
+
+        //stores a floating point number from -3.4 * 1038 to 3.4 * 1038 and takes 4 bytes
+        float f = 5.33f;
+
+        //stores a single UTF-16 encoded character and takes 2 bytes, so the range of stored values is 0 to 65535
+        char c = 43;
+        System.out.println(c);
+
     }
 }
