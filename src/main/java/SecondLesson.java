13cdec2f39789cd2edbef7ad6627421fa4db624b
Dimoniss <hryndmytro@gmail.com>
2021-02-21 19:40:03 +0100

    Lesson 2 Task 3 Set an array [1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1] to loop through it, and multiply numbers less than 6 by 2

--- src/main/java/SecondLesson.java
+++ src/main/java/SecondLesson.java
@@ -22,7 +22,16 @@ public class SecondLesson {
             array2[i] = count;
             count += 3;
         }
+        FirstLesson.splitTasks(2);
         System.out.println(Arrays.toString(array2));
+
+        //--------------------Task 3
+        int[] array3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < array3.length; i++) {
+            if (array3[i]<6) array3[i] = array3[i] * 2;
+        }
+        FirstLesson.splitTasks(3);
+        System.out.println(Arrays.toString(array3));
 
     }
 }
