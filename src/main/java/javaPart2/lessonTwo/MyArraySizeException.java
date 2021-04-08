4e8d440f35ef48065443ee77600b4c5660cf9de8
Dimoniss <hryndmytro@gmail.com>
2021-04-08 18:12:42 +0200

    Java 2 Lesson 2 1. Write a method, the input of which is a two-dimensional string array of 4x4 size; when an array of a different size is supplied, an exception MyArraySizeException must be thrown. 2. Next, the method must go through all the elements of the array, convert to int, and sum. If the transformation failed in some element of the array (for example, there is a character or text instead of a number in a cell), an exception MyArrayDataException should be thrown, detailing which cell contains incorrect data. 3. In the main () method, call the resulting method, handle possible exceptions MySizeArrayException and MyArrayDataException, and display the result of the calculation.

---
+++ src/main/java/javaPart2/lessonTwo/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package javaPart2.lessonTwo;
+
+public class MyArraySizeException extends Exception {
+    public String toString() {
+        return "Error, incorrect matrix size (correct matrix[4][4])";
+    }
+}
