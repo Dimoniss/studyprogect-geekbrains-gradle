543988eba19829940bcd6ec4590c019fa4ad1cc1
Dimoniss <hryndmytro@gmail.com>
2021-05-23 12:27:13 +0200

    Java 3 Lesson 5 All competitors must start at the same time, despite the fact that each of them takes a different time to prepare. More than half of the participants cannot enter the tunnel at the same time (convention). Try to sync it all up. Only after everyone has finished the race, you need to issue an announcement of the end. You can adjust the classes (including the machine constructor) and add class objects from the util.concurrent package.

--- src/main/java/javaPart3/lesson5/Car.java
+++ src/main/java/javaPart3/lesson5/Car.java
@@ -1,39 +1,51 @@
 package javaPart3.lesson5;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
 
 public class Car implements Runnable {
     private static int CARS_COUNT;
-    static {
-        CARS_COUNT = 0;
-    }
     private Race race;
     private int speed;
     private String name;
-    private CyclicBarrier cb;
-    public String getName() {
-        return name;
+    private CyclicBarrier cyclicBarrierReady;
+    private CountDownLatch countDownLatchFinish;
+
+    static {
+        CARS_COUNT = 0;
     }
-    public int getSpeed() {
-        return speed;
-    }
-    public Car(Race race, int speed) {
+
+    public Car(Race race, int speed, CyclicBarrier ready, CountDownLatch finish) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.cyclicBarrierReady = ready;
+        this.countDownLatchFinish = finish;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+
     @Override
     public void run() {
         try {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            cyclicBarrierReady.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+        countDownLatchFinish.countDown();
     }
 }
--- src/main/java/javaPart3/lesson5/MainClass.java
+++ src/main/java/javaPart3/lesson5/MainClass.java
@@ -8,20 +8,25 @@ public class MainClass {
     public static final int CARS_COUNT = 4;
     public static void main(String[] args) {
 
-        //final CountDownLatch startLatch = new CountDownLatch(CARS_COUNT);
-        CyclicBarrier cyclicBarrier = new CyclicBarrier(CARS_COUNT);
+        CountDownLatch countDownLatchFinish = new CountDownLatch(CARS_COUNT);
+        CyclicBarrier cyclicBarrierReady = new CyclicBarrier(CARS_COUNT, () -> System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!"));
 
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Race race = new Race(new Road(60), new Tunnel(CARS_COUNT/2), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cyclicBarrierReady, countDownLatchFinish);
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
 
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        try {
+            countDownLatchFinish.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
